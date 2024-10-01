10d0c247951236278cee8f610dc79e26d271fc99
Basel638 <basselsameh2004@gmail.com>
2024-10-01 17:35:53 +0300

    Add project files.

---
+++ Talabat.APIs/Program.cs
@@ -0,0 +1,84 @@
+using Microsoft.EntityFrameworkCore;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Repository;
+using Talabat.Repository.Data;
+
+namespace Talabat.APIs
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var webApplicationBuilder = WebApplication.CreateBuilder(args);
+
+			#region Configure Services
+			// Add services to the DI container.
+
+			webApplicationBuilder.Services.AddControllers();
+			// Register Required Web APIs Services to the DI Container
+
+
+
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			webApplicationBuilder.Services.AddEndpointsApiExplorer();
+			webApplicationBuilder.Services.AddSwaggerGen(); 
+
+
+
+			webApplicationBuilder.Services.AddDbContext<StoreContext>(options =>
+			{
+				options.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection"));
+			
+			});
+
+			webApplicationBuilder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+			#endregion
+
+			var app = webApplicationBuilder.Build();
+
+			using var scope = app.Services.CreateScope();
+
+			var services= scope.ServiceProvider;
+
+			var _dbContext = services.GetRequiredService<StoreContext>();
+			// Ask CLR for Creating Object from DbContext Explicitly
+
+			var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+			try
+			{
+
+				await _dbContext.Database.MigrateAsync();   // Update-Database
+
+				await StoreContextSeed.SeedAsync(_dbContext); // Data Seeding
+			}
+			catch (Exception ex)
+			{
+				var logger = loggerFactory.CreateLogger<Program>();
+				logger.LogError(ex, "an error has been occured during apply the migrations");
+			}
+
+			#region Configure Kestrel Middlewares
+
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			
+
+			app.MapControllers();
+
+			#endregion
+
+			 
+			app.Run(); 
+		}
+	}
+}
---
+++ Talabat.Repository/Data/StoreContextSeed.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Repository.Data
+{
+	public static class StoreContextSeed
+	{
+		public async static Task SeedAsync(StoreContext _dbContext)
+		{
+			if (_dbContext.ProductBrands.Count()==0)
+			{
+				var brandsData = File.ReadAllText("../Talabat.Repository/Data/DataSeed/brands.json");
+
+				var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+
+				if (brands is not null && brands.Count() > 0)
+				{
+
+					foreach (var brand in brands)
+					{
+						_dbContext.Set<ProductBrand>().Add(brand);
+					}
+					await _dbContext.SaveChangesAsync();
+				} 
+			}
+			
+			if (_dbContext.ProductCategories.Count()==0)
+			{
+				var categoriessData = File.ReadAllText("../Talabat.Repository/Data/DataSeed/categories.json");
+
+				var categories = JsonSerializer.Deserialize<List<ProductCategory>>(categoriessData);
+
+				if (categories is not null && categories.Count() > 0)
+				{
+
+					foreach (var categorie in categories)
+					{
+						_dbContext.Set<ProductCategory>().Add(categorie);
+					}
+					await _dbContext.SaveChangesAsync();
+				} 
+			}
+
+
+		}
+	}
+}
